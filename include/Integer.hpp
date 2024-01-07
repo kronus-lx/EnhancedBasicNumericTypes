3f6d5884b6e8779f8bb8476115159e865bab20c8
kronus-lx <joelem2@hotmail.com>
2024-01-07 19:20:51 +0000

    initial integer class

---
+++ include/Integer.hpp
@@ -0,0 +1,57 @@
+#include <string_view>
+#pragma once
+    /**
+    * @brief Integer Class Encapsulatuing Integer Methods and Attributes
+    */
+    class Integer
+    {
+        private:
+            int _c_integer;
+            unsigned int _size;
+        public:
+            /**
+            *   @brief Standard Constructor invoke new object passing std int value
+            *   @param standard cpp int value
+            */
+            Integer(int c_type_integer);
+            /**
+            *   @brief Standard Constructor invoke new object passing std int value
+            *   @param instance of Integer class
+            */
+            Integer(const Integer &integer) noexcept;
+            /**
+            * @brief Assignement operator
+            * @param instance of Integer class
+            */
+            Integer& operator=(const Integer &integer) noexcept;
+            
+            /**
+            * @brief signed value of integer
+            * @return signed value of int
+            */
+            int value() const ;
+            
+            /**
+            * @brief signed size in bytes
+            * @return signed size in bytes
+            */
+            unsigned int size() const ;
+
+            /**
+            * @brief string representation of binary value of int
+            * @return string_view (C++17) value of binary value of int
+            */
+            std::string_view binary() const ; 
+            
+            /**
+            * @brief is a prime number
+            * @return boolean state (0 if no | 1 if yes )
+            */
+            bool isPrime() const ;
+
+            /**
+            * @brief returns the factorial of the provided integer
+            * @return factorial (long) of provided integer
+            */
+            long Factorial() const ;
+    };
---
+++ src/Integer.cpp
@@ -0,0 +1,73 @@
+#include <Integer.hpp>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+Integer::Integer(int c_type_integer){
+    _c_integer = c_type_integer;
+    _size = sizeof(this->_c_integer);
+}
+
+Integer::Integer(const Integer &integer ) noexcept 
+    :_c_integer(integer._c_integer),_size(integer._size) 
+{}
+
+Integer& Integer::operator=(const Integer& integer) noexcept {
+    _c_integer = integer._c_integer;
+    _size = sizeof(int);
+    return *this;
+}
+
+int Integer::value() const {
+    return this->_c_integer;
+}
+
+unsigned int Integer::size() const {
+    return this->_size;
+}
+
+std::string_view Integer::binary() const {
+    std::string strOutput;
+    std::vector<unsigned char> binary;   
+    int v = _c_integer;
+    if(v == 0) binary.push_back(0);
+    else {
+        while(v != 0){
+            unsigned char y = static_cast<unsigned char>( v % 2);
+            binary.push_back(y);
+            v /= 2;
+        }
+    }
+    std::reverse(binary.begin(), binary.end());
+    for(auto i = binary.begin(); i != binary.end(); i++){
+        strOutput += std::to_string(*i);
+    }
+    
+    std::string_view output = strOutput;
+
+    return output;
+} 
+
+bool Integer::isPrime() const {
+    int x = _c_integer;
+    bool isPrime = true;
+    if(x == 0 || x == 1){
+        isPrime = false;
+    }
+    for(int i = 2; i <= x/2; ++i ){
+        if(x % i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime;
+} 
+
+long Integer::Factorial() const {
+    int x = _c_integer;
+    long factorial = 0;
+    for(int i = 1; i <= x; ++i){
+        factorial *= i;
+    }
+    return factorial;
+}
