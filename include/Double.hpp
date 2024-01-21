4e10fc39717d42884700ccb8ba37a748dc981238
kronus-lx <joelem2@hotmail.com>
2024-01-21 17:50:40 +0000

    added two signatures for double class

--- include/Double.hpp
+++ include/Double.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cinttypes>
+
 class Double 
 {
     private:
@@ -21,9 +23,18 @@ class Double
         *   @param standard cpp int value
         */
         Double& operator=(double _cpp_type_double) noexcept ;
-         /**
+        /**
         *   @brief Standard Constructor invoke new object passing std int value
         *   @param standard cpp int value
         */
         Double& operator=(const Double &double) noexcept ;
+        /**
+        *   @brief Standard Constructor invoke new object passing std int value
+        *   @param standard cpp int value
+        */
+        int toPrimitiveInt( ) const ;
+
+        int32_t toInt32() const ;
+
+        int64_t toInt64() const ;
 };
